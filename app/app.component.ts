341a8717fa68b6f5f7f25751d359060220d526a5
Thomas Wiegand <twiegand@rogers.com>
2016-10-29 15:30:37 -0400

    End of Tutorial

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,14 +8,16 @@ import {HeroService} from './hero.service';
 
 @Component(
     {
+        moduleId: module.id,
         selector: 'my-app',
         template: `<h1>{{title}}</h1>
                     <nav>
-                         <a routerLink="/dashboard">Dashboard</a>
-                         <a routerLink="/heroes">Heroes</a>
+                         <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+                         <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
                     </nav>
                     <router-outlet></router-outlet>
                     `,
+        styleUrls: ['app.component.css'],
         providers: [HeroService]
     }
 )
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -14,6 +14,7 @@ import { HeroService } from './hero.service';
     moduleId: module.id,
     selector: 'my-hero-detail',
     templateUrl: 'hero-detail.component.html',
+    styleUrls: ['hero-detail.component.css']
 })
 
 export class HeroDetailComponent implements OnInit{
@@ -37,5 +38,11 @@ export class HeroDetailComponent implements OnInit{
     goBack(): void {
         this.location.back();
     }
+
+    save(): void {
+        this.heroService.update(this.hero)
+            .then(() => this.goBack());
+    }
+
 
 }
